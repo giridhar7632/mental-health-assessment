79e6fb8f1f0e32b52767d01ebfc7a0f302fe18f6
Talla Giridhar <giridhar.talla2002@gmail.com>
2021-02-27 20:44:18 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello World! <span role="img" aria-label="waving-hand">👋</span></h1>
+    </div>
+  );
+}
